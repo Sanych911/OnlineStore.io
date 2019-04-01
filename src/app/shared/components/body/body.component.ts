0cd7a8c1fb2482177692aaac46e12aa567c804ed
Александр Баханов <m-21sanych@rambler.ru>
2019-04-01 15:57:38 +0300

    2

--- src/app/shared/components/body/body.component.ts
+++ src/app/shared/components/body/body.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
 import { Input} from '@angular/core';
 
 
@@ -11,13 +11,25 @@ import { Input} from '@angular/core';
 providers:[]
 })
 export class BodyComponent  {
-  
+  id:number;
   @Input() itemsBody: any;
   
-  
+  visibil:boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  test(val:number,itemsBody){
+   // let isFound = itemsBody.some(_ => _.itemsBody_id == val);
+   
+  let res=itemsBody.some(
+   function(itemsBody)
+       { return itemsBody.id == val});
+  return res;
+  }
+  
+  
+   
 }
